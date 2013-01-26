c3f43e2ad35fff7f7ebe18ee7214db8f70df2e54
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-26 12:48:58 -0800

    Extract a vector of frames from a controller - needed to detect gestures

--- src/clojure_leap/controller.clj
+++ src/clojure_leap/controller.clj
@@ -10,6 +10,10 @@
                        (min history-count history-count-max)
                        0)))
 
+;; Perhaps a future API will suport this natively
+(defn frames [^Controller controller history-count]
+  (vec (keep #(try (frame controller %) (catch Exception e nil)) (range history-count))))
+
 (defn ^ScreenList screens [^Controller controller]
   (.calibratedScreens controller))
 
@@ -20,9 +24,13 @@
 (defn connected? [^Controller controller]
   (.isConnected controller))
 
-(defn controller-map [^Controller controller]
-  {:controller controller
-   :frame (.frame controller)
-   :screens (screens controller)
-   :state :frame})
+(defn controller-map
+  ([^Controller controller]
+   (controller-map controller nil))
+  ([^Controller controller listener]
+   {:controller controller
+    :frame (.frame controller)
+    :screens (screens controller)
+    :state :frame
+    :listener listener}))
 
